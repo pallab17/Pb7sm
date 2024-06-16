7f45cedc749cf7bd6ade39411b871aa7b4ac2000
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-06-16 15:17:05 +0530

    react-router-dom added,user pg,post pg,pics in public folder

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import { Button } from "@chakra-ui/react"
 import { Container } from "@chakra-ui/react"
+import {Route,Routes} from "react-router-dom"
+import UserPage from "../pages/UserPage"
+import PostPage from "../pages/PostPage"
 
 
 function App() {
  
   return (
     <Container maxW="620px">
-   <Button>
-    Hello
-   </Button>
+    <Routes>
+      <Route path="/:username" element={<UserPage/>}  />
+      <Route path="/:username/post/:pid" element={<PostPage/>}  />
+    </Routes>
+ 
     </Container>
   )
 }
---
+++ pb7.md
@@ -0,0 +1,5 @@
+chakra ui addition
+chakra ui testing using button provided by chaakra ui
+react-router dom addition
+user page post-page addition
+public folder e pic addition from github of as a programmer
