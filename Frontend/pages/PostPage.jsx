81f8692247eb9f5d9d8697a62a42f8d73f0e3620
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-06-17 06:44:19 +0530

    user-post pg post part done

--- Frontend/pages/PostPage.jsx
+++ Frontend/pages/PostPage.jsx
@@ -1,10 +1,47 @@
 //import React from 'react'
+import { useState } from "react"
+import { Flex,Avatar,Text,Image, Box, Center} from "@chakra-ui/react"
+import {BsThreeDots} from "react-icons/bs"
+import Actions from "../components/Actions"
 
 const PostPage = () => {
+	const[liked,setLiked]=useState(false);
   return (
-    <div>
-      
-    </div>
+    <>
+      <Flex>
+				<Flex w={"full"} alignItems={"center"} gap={3}>
+					<Avatar src="./zuck-avatar.png" size={"md"} name='Mark Zuckerberg' />
+					<Flex>
+						<Text fontSize={"sm"} fontWeight={"bold"}>
+						zuck-pb7
+						</Text>
+						<Image src='/verified.png' w='4' h={4} ml={4} />
+					</Flex>
+				</Flex>
+				<Flex gap={4} alignItems={"center"}>
+					<Text fontSize={"xs"} width={36} textAlign={"right"} color={"gray.light"}>
+						1d 
+					</Text>
+
+					<BsThreeDots/>
+				</Flex>
+			</Flex>
+			<Text my={3}>lets talk about us</Text>
+			<Box borderRadius={6} overflow={"hidden"} border={"1px solid"} borderColor={"gray.light"}>
+					<Image src={"/post1.png"} w={"full"} />
+				</Box>
+				<Flex gap={3} my={3}>
+				<Actions  liked={liked} setLiked={setLiked}/>
+			</Flex>
+			<Flex gap={2} alignItems={"center"}>
+			<Text color={"gray.light"} fontSize={"sm"}  >299 replies</Text>
+			<Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray.light"}></Box>
+			<Text color={"gray.light"} fontSize={"sm"}  >
+			{0 + (liked ? 1 : 0)}likes
+			</Text>
+
+			</Flex>
+    </>
   )
 }
 
