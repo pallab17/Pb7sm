a6fc498cfb087eb9b99aab1d393303bdce122547
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-06-18 04:43:59 +0530

    creating the routes

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/connectDb.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 connectDB();
@@ -9,4 +10,10 @@ const app =express();
 
 const PORT = process.env.PORT || 5000;
 
+// Middlewares
+app.use(express.json({ limit: "50mb" })); // To parse JSON data in the req.body
+app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
+app.use(cookieParser());
+// Routes
+app.use("/api/users", userRoutes);
 app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
