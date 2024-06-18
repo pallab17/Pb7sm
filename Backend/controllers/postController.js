c0dcd1deeedc92d48e70166be2583fedac20e634
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-06-18 07:44:52 +0530

    delete-post  api created and tested using postman

--- Backend/controllers/postController.js
+++ Backend/controllers/postController.js
@@ -51,5 +51,28 @@ const getPost = async (req, res) => {
 		res.status(500).json({ error: err.message });
 	}
 };
+const deletePost = async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+		if (!post) {
+			return res.status(404).json({ error: "Post not found" });
+		}
 
-export {createPost,getPost}
+		if (post.postedBy.toString() !== req.user._id.toString()) {
+			return res.status(401).json({ error: "Unauthorized to delete post" });
+		}
+
+		// if (post.img) {
+		// 	const imgId = post.img.split("/").pop().split(".")[0];
+		// 	await cloudinary.uploader.destroy(imgId);
+		// }
+
+		await Post.findByIdAndDelete(req.params.id);
+
+		res.status(200).json({ message: "Post deleted successfully" });
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+};
+
+export {createPost,getPost,deletePost}
--- Backend/routes/postRoutes.js
+++ Backend/routes/postRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { createPost,getPost } from "../controllers/postController.js";
+import { createPost,getPost,deletePost } from "../controllers/postController.js";
 import protectRoute from "../middlewares/protectRoute.js"
 
 const router = express.Router();
 router.post("/create", protectRoute, createPost);
 router.get("/:id", getPost);
-
+router.delete("/:id", protectRoute, deletePost);
 export default router;
