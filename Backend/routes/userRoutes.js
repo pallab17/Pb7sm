dda40d1db410de60517b6d9a731fb5e1d4576143
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-06-18 06:10:00 +0530

    follow/unfollow  api is tested using postman part1

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { signupUser,loginUser,logoutUser } from "../controllers/userController.js";
+import { signupUser,loginUser,logoutUser,followUnFollowUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
