957c3d999c05e219c9385be90f55250d89105592
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-06-18 08:12:44 +0530

    replytopost  api created and tested using postman

--- Backend/routes/postRoutes.js
+++ Backend/routes/postRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createPost,getPost,deletePost,likeUnlikePost } from "../controllers/postController.js";
+import { createPost,getPost,deletePost,likeUnlikePost,replyToPost } from "../controllers/postController.js";
 import protectRoute from "../middlewares/protectRoute.js"
 
 const router = express.Router();
@@ -7,4 +7,5 @@ router.post("/create", protectRoute, createPost);
 router.get("/:id", getPost);
 router.delete("/:id", protectRoute, deletePost);
 router.post("/like/:id", protectRoute, likeUnlikePost);
+router.post("/reply/:id", protectRoute, replyToPost);
 export default router;
