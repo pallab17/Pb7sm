a3fba0bf6bb6bbdc6325726bf0dc21e2803944dd
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-06-18 07:02:27 +0530

    get-user-profile  api is tested using postman completed

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,6 +1,34 @@
 import User from "../models/userModel.js"
 import bcrypt from "bcryptjs";
 import generateTokenAndSetCookie from "../utils/helper/generateTokenAndSetCookie.js";
+import mongoose from "mongoose";
+
+
+const getUserProfile = async (req, res) => {
+	// We will fetch user profile either with username or userId
+	// query is either username or userId
+	const { query } = req.params;
+
+	try {
+		let user;
+
+		// query is userId
+		if (mongoose.Types.ObjectId.isValid(query)) {
+			user = await User.findOne({ _id: query }).select("-password").select("-updatedAt");
+		} else {
+			// query is username
+			user = await User.findOne({ username: query }).select("-password").select("-updatedAt");
+		}
+
+		if (!user) return res.status(404).json({ error: "User not found" });
+
+		res.status(200).json(user);
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+		console.log("Error in getUserProfile: ", err.message);
+	}
+};
+
 
 const signupUser = async (req, res) => {
 	try {
@@ -165,4 +193,6 @@ const updateUser = async (req, res) => {
 	}
 };
 
-export {signupUser,loginUser,logoutUser,followUnFollowUser,updateUser};
+
+
+export {signupUser,loginUser,logoutUser,followUnFollowUser,updateUser,getUserProfile};
