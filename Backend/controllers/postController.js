2bdf4a07d9e4f88438a30acf09577901e6775adf
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-06-18 07:37:04 +0530

    create-post and getpost api created and tested using postman

--- Backend/controllers/postController.js
+++ Backend/controllers/postController.js
@@ -1,4 +1,55 @@
+import User from "../models/userModel.js";
+import Post from "../models/postModel.js"
+ 
 const createPost = async (req, res) => {
+	try {
+		const { postedBy, text } = req.body;
+		let { img } = req.body;
+
+		if (!postedBy || !text) {
+			return res.status(400).json({ error: "Postedby and text fields are required" });
+		}
+
+		const user = await User.findById(postedBy);
+		if (!user) {
+			return res.status(404).json({ error: "User not found" });
+		}
+
+		if (user._id.toString() !== req.user._id.toString()) {
+			return res.status(401).json({ error: "Unauthorized to create post" });
+		}
+
+		const maxLength = 500;
+		if (text.length > maxLength) {
+			return res.status(400).json({ error: `Text must be less than ${maxLength} characters` });
+		}
+
+		// if (img) {
+		// 	const uploadedResponse = await cloudinary.uploader.upload(img);
+		// 	img = uploadedResponse.secure_url;
+		// }
+
+		const newPost = new Post({ postedBy, text, img });
+		await newPost.save();
+
+		res.status(201).json({message:"Post Created successfully",newPost});
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+		console.log(err);
+	}
+};
+const getPost = async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		if (!post) {
+			return res.status(404).json({ error: "Post not found" });
+		}
+
+		res.status(200).json(post);
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
 };
 
-export {createPost}
+export {createPost,getPost}
--- Backend/routes/postRoutes.js
+++ Backend/routes/postRoutes.js
@@ -1,6 +1,9 @@
 import express from "express";
-import { createPost } from "../controllers/postController";
+import { createPost,getPost } from "../controllers/postController.js";
+import protectRoute from "../middlewares/protectRoute.js"
 
 const router = express.Router();
 router.post("/create", protectRoute, createPost);
+router.get("/:id", getPost);
+
 export default router;
