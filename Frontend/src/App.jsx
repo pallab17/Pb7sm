1989153526e82828a5640d2543f4a5ae3ae3329e
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-06-19 09:58:23 +0530

    frontend server at 3000 made

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,9 @@
 //import { Button } from "@chakra-ui/react"
 import { Container } from "@chakra-ui/react"
 import {Route,Routes} from "react-router-dom"
-import UserPage from "../pages/UserPage"
-import PostPage from "../pages/PostPage"
-import Header from "../components/Header"
+import UserPage from "../src/pages/UserPage"
+import PostPage from "../src/pages/PostPage"
+import Header from "../src/components/Header"
 
 
 function App() {
@@ -12,6 +12,7 @@ function App() {
     <Container maxW="620px">
     <Header/>
     <Routes>
+    <Route path='/' element={ <HomePage /> } />
       <Route path="/:username" element={<UserPage/>}  />
       <Route path="/:username/post/:pid" element={<PostPage/>}  />
     </Routes>
