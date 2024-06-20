0316e6e22dfcc73669407227cf7341fdcd7c8e36
pallab17 <91554568+pallab17@users.noreply.github.com>
2024-06-20 13:33:07 +0530

    changed

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,4 @@
-import path from "path";
+//import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
@@ -13,9 +13,9 @@ connectDB();
 
 const app =express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 cloudinary.config({
 	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -31,13 +31,13 @@ app.use(cookieParser());
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+// 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-	// react app
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
+// 	// react app
+// 	app.get("*", (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+// 	});
+// }
 
 app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
